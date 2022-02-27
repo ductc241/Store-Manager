11aa82dfc6d1b7ab9660ab63b62dd9a8bc2e159b
ductc241 <tacongduc123@gmail.com>
2022-02-27 21:24:13 +0700

    Upload project: Store Manager

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Application from "./containers/Application"
+
+// Redux store
+
+// Style
+import 'antd/dist/antd.css';
+import './style.css'
+
+
+const app = () => {
+  return (
+    <Application />
+  )
+}
+
+export default app
---
+++ src/containers/Application/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom';
+
+import Login from '../Login/Login';
+
+const Application = () => {
+    return (
+        <div className="App">
+          <Login />
+        </div>
+    )
+}
+
+export default Application
